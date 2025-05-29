71ffb69e91e968a2fada16facaaa3c98a0039404
Kimomo <t.kashi0113@gmail.com>
2025-05-29 22:27:33 +0900

    no message

--- docs/index.7f585.js
+++ docs/index.7f585.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.5f1c3.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
