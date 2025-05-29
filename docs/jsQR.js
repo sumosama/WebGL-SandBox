a2ce06bd8a0002af5a5caed4cb16715bbcd13460
Kimomo <t.kashi0113@gmail.com>
2025-05-29 22:32:22 +0900

    no message

--- docs/jsQR.js
+++ docs/jsQR.js
@@ -890,7 +890,7 @@ var AlphanumericCharacterCodes = [
     "9", "A", "B", "C", "D", "E", "F", "G", "H",
     "I", "J", "K", "L", "M", "N", "O", "P", "Q",
     "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-    " ", "$", "%", "*", "+", "-", "index.7f585.js", index.7f585.js"/", ":",
+    " ", "$", "%", "*", "+", "-", ".", "/", ":",
 ];
 function decodeAlphanumeric(stream, size) {
     var bytes = [];
